152c6b0a5f36bf73c2505eae9bedb719134d2c01
Rafael Matthes <rafael.matthes@live.com>
2022-01-03 14:54:47 -0300

    fix

--- HttpTrigger-sendemail/__init__.py
+++ HttpTrigger-sendemail/__init__.py
@@ -6,9 +6,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     receiver = req.params.get('receiver_email')
     text = req.params.get('aditional_text','')
     link = req.params.get('link', None)
+    html = req.params.get('html_text', None)
 
     if receiver:
-        this_email = EmailSender(receiver, link=link, text= text)
+        this_email = EmailSender(receiver, link=link, text= text, html=html)
         return func.HttpResponse('', status_code=this_email)
     else:
         logging.error(f'Request without receiver email')
